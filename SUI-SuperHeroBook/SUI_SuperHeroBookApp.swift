26052e037c84bcb48954139cafe2e31bb94d9f2e
hasanuysaal <hsnuysaal@icloud.com>
2022-09-19 12:05:55 +0300

    Initial Commit

---
+++ SUI-SuperHeroBook/SUI_SuperHeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_SuperHeroBookApp.swift
+//  SUI-SuperHeroBook
+//
+//  Created by Hasan Uysal on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUI_SuperHeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
