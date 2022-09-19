15dfe8104c57548d9fb53bfc8194b04ce2c3effc
hasanuysaal <hsnuysaal@icloud.com>
2022-09-19 14:28:07 +0300

    List View created to change screen with data using NavigationView

---
+++ SUI-SuperHeroBook/ListView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  SUI-SuperHeroBook
+//
+//  Created by Hasan Uysal on 19.09.2022.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        NavigationView{
+            List(superHeroArr) { superHero in
+                NavigationLink {
+                    DetailsView(choosedSuperHero: superHero)
+                } label: {
+                    Text(superHero.name)
+                }
+
+            }.navigationTitle("SuperHero Book")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
--- SUI-SuperHeroBook/SUI_SuperHeroBookApp.swift
+++ SUI-SuperHeroBook/SUI_SuperHeroBookApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SUI_SuperHeroBookApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListView()
         }
     }
 }
